1537851e362c558129f72346c2ab9023f21d93be
Ashish Garg <ashgarg@publicisgroupe.net>
2022-03-28 11:51:13 +0100

    Initial commit

---
+++ src/main/java/com/example/mysqlserver/MysqlserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlserverApplication.class, args);
+	}
+
+}
