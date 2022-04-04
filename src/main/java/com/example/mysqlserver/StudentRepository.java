56d66fc9d95e5176a3be8c345b1c89dd2e9b8d1b
Ashish Garg <ashgarg@publicisgroupe.net>
2022-04-04 14:58:49 +0100

    updated repo

--- src/main/java/com/example/mysqlserver/StudentRepository.java
+++ src/main/java/com/example/mysqlserver/StudentRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "student", collectionResourceRel = "student")
-
 public interface StudentRepository extends PagingAndSortingRepository<Student, Integer> {
 }
