c9f7c435294a53e57a5bd47fd20902207ba21e27
Ashish Garg <ashgarg@publicisgroupe.net>
2022-04-08 16:21:38 +0100

    Application UI

--- src/main/java/com/example/mysqlserver/TaggingController.java
+++ src/main/java/com/example/mysqlserver/TaggingController.java
@@ -6,16 +6,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -23,9 +24,8 @@ import java.util.Map;
 //@RestController
 @Controller
 public class TaggingController {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdHHmmss");
-
     private final TaggingService taggingService;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdHHmmss");
 
     public TaggingController(TaggingService taggingService) {
         this.taggingService = taggingService;
@@ -61,7 +61,7 @@ public class TaggingController {
 
         //Closing the document
         pdDocument.close();
-        String filename = document.getOriginalFilename().split("\\.")[0] + "_" + LocalDateTime.now().format(formatter)+ ".pdf";
+        String filename = document.getOriginalFilename().split("\\.")[0] + "_" + LocalDateTime.now().format(formatter) + ".pdf";
         var headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
 
@@ -70,5 +70,12 @@ public class TaggingController {
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(new InputStreamResource(new ByteArrayInputStream(out.toByteArray())));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView model = new ModelAndView("welcome");
+        model.addObject("error", "internal error while processing");
+        return model;
     }
 }
