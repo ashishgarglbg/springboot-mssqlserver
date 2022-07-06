edc77ac959333b825bab39e5fe235e3ffb0b1994
Ashish Garg <ashgarg@publicisgroupe.net>
2022-07-06 14:14:46 +0100

    update welcome html

--- src/main/java/com/example/mysqlserver/TaggingController.java
+++ src/main/java/com/example/mysqlserver/TaggingController.java
@@ -3,6 +3,7 @@ package com.example.mysqlserver;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -65,17 +69,41 @@ public class TaggingController {
         var headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
 
+//        return ResponseEntity
+//                .ok()
+//                .headers(headers)
+//                .contentType(MediaType.APPLICATION_PDF)
+//                .contentLength(out.size())
+//                .body(new InputStreamResource(new ByteArrayInputStream(out.toByteArray())));
+
         return ResponseEntity
                 .ok()
                 .headers(headers)
-                .contentType(MediaType.APPLICATION_PDF)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(out.size())
                 .body(new InputStreamResource(new ByteArrayInputStream(out.toByteArray())));
+        //https://github.com/eligrey/FileSaver.js/issues/238#issuecomment-224572273
+        //https://stackoverflow.com/questions/1999607/download-and-open-pdf-file-using-ajax
     }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex) {
         ModelAndView model = new ModelAndView("welcome");
         model.addObject("error", "internal error while processing");
+        return model;
+    }
+
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ModelAndView handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
+        ModelAndView model = new ModelAndView("welcome");
+        if ("document".equalsIgnoreCase(ex.getRequestPartName())) {
+            model.addObject("documentError", "Please select a report to tag");
+        }
+        else if ("mapping".equalsIgnoreCase(ex.getRequestPartName())) {
+            model.addObject("mappingError", "Please select a mapping sheet");
+        } else {
+            model.addObject("error", "internal error while processing");
+        }
         return model;
     }
 }
